6ed9ad8e45f4da9410ae125de162582f0f26265e
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2022-11-24 19:51:35 +0530

    no changes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./app")
-require("dotenv").config();
+
 const PORT = process.env.PORT 
 
 app.listen(PORT, () => {
+    console.log(PORT);
     console.log(`App is running at ${PORT} `);
 })
